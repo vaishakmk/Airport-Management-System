c9704ffc20ccdb69484e860a8c835b5a63a7b2da
vaishakmk <37268852+vaishakmk@users.noreply.github.com>
2022-11-19 03:26:26 -0800

    baggage assignment by airport employee implemented

--- server/controllers/airport_employee.js
+++ server/controllers/airport_employee.js
@@ -30,4 +30,56 @@ exports.set_gates = (req,res) =>{
     })
     .catch(err => res.status(404).json({error : 'Please provide correct terminal and gate info' }));
     return;     
-}
+};
+
+exports.get_flights = (req,res)=> {
+    Flights.find({arr_dep:'arrival'})
+    .exec()
+    .then(flights=>{
+        res.json(flights);
+        return;
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+    return;
+};
+
+
+
+exports.set_baggage = (req,res) => {
+    Flights.find({baggage:req.body.baggage})
+    .exec()
+    .then(baggageclaim => {
+        if(baggageclaim.length<4){
+            Flights.find({flight_num:req.body.flight_num, arr_dep:'arrival'})
+            .exec()
+            .then(flights => {
+                if (flights.length >0){
+                    let this_flight = flights[0];
+                    if(this_flight.baggage===undefined){
+                        this_flight.baggage = req.body.baggage;
+                        this_flight.save()
+                        .then(() => res.json('Baggage Assigned Successfully'))
+                        .catch(err => res.status(400).json({'Some Error':err}));
+                        return;
+                    }else{
+                        res.status(409).json({error:'This Flight has already been assigned a baggage counter'});
+                        return;
+                    }
+                   
+                }else{
+                    res.status(400).json({'Error':'Please re-enter the correct flight information'});
+                }
+                return; 
+            })
+            .catch(err => res.status(400).json({'Error': err}));
+            return;
+
+        }
+        else{
+            res.status(409).json({error:'This baggage counter has already been assigned to 4 flights. Please assign another counter'});
+            return;
+
+        }
+    })
+    
+};
