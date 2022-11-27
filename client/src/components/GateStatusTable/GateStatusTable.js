66602935387a593c00b7ca5a272b68eb2c6b0451
Divija Naredla <divija.naredla@sjsu.edu>
2022-11-27 02:44:02 -0800

    Adding GateStatusTable component to show Terminal-Gate Status

---
+++ client/src/components/GateStatusTable/GateStatusTable.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import "./GateStatusTable.css";
+
+function BaggageTable({ inventory_api_url }) {
+    const [data, setData] = useState([]);
+
+    const fetchInventory = () => {
+        fetch("http://localhost:5001/airport_employee/gates")
+            .then(res => res.json())
+            .then(json => setData(json));
+
+    }
+
+    useEffect(() => {
+        fetchInventory();
+    }, []);
+
+    return (
+        <div className="table-wrapper">
+            <h1>Terminal-Gate Status Details</h1>
+            <table className="fl-table">
+                <thead>
+                    <tr>
+                        
+                        <th>Terminal</th>
+                        <th>Gate</th>
+                        <th>Status</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((item) => (
+                            <tr key={item.id}>
+                                
+                                <td>{item.terminal}</td>
+                                <td>{item.gate_num}</td>
+                                <td>{item.gate_status}</td>
+
+
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default BaggageTable;
