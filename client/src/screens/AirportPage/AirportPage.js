78456d716e84577930240efdb002e6aa2effb16e
Aksh-Narayana <99863530+Aksh-Narayana@users.noreply.github.com>
2022-11-29 16:29:31 -0800

    Added Random Gate Assignment
    Added logic and button to randomly assign gates.

--- client/src/screens/AirportPage/AirportPage.js
+++ client/src/screens/AirportPage/AirportPage.js
@@ -6,6 +6,15 @@ import { Link } from 'react-router-dom';
 import StaticTable from '../../components/StaticTable/StaticTable';
 
 export default function AirportPage() {
+    const assignRandomGate = () => {
+        e.preventDefault();
+        fetch('http://localhost:5001/gates/random_assignment', {
+            method: 'POST'
+        })
+        .then(res => res.json())
+        .catch(err => console.log(err));
+    };
+
     return (
         <div>
         <Button variant="primary">
@@ -13,6 +22,9 @@ export default function AirportPage() {
         </Button>
         <Button variant="primary">
             <Link to="/airport/baggage">Baggage Carousel Assignment</Link>
+        </Button>
+        <Button variant="primary">
+            <Link onClick={assignRandomGate}>Random Gate Assign Gate</Link>
         </Button>
         <StaticTable/>
         </div>
