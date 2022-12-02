e1194ca8da3068962b2341b033a175df2670a832
Divija Naredla <divija.naredla@sjsu.edu>
2022-12-01 23:16:02 -0800

    Header logo fix

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import "./Header.css";
+// import logo from '../images/logo.jpeg';
 
 
 
 const Header = () => {
   const [logout, setLogout] = React.useState(false);
+  
+  
   React.useEffect(() => {
     
     if(localStorage.getItem('token')) {
@@ -16,12 +19,22 @@ const Header = () => {
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">San Jose Airport</Navbar.Brand>
+        {/* <Navbar.Brand href="#home">San Jose Airport</Navbar.Brand> */}
+        <Navbar.Brand>
+          <div className="logo">
+          
+
+          </div>
+  </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="topnav-right">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+            
+            {
+
+            }
             {
               logout && <Nav.Link href="/">logout</Nav.Link>
             }
